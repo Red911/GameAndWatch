8c5a36d00bc178c3e312ffde547f7960495e1619
Julien <julien.da_silva_soares@edu.devinci.fr>
2022-09-08 11:50:47 +0200

    Push proto

---
+++ Assets/Julien/Script/ButtonPress.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    private bool isPressed = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Press()
+    {
+        if (isPressed)
+        {
+            
+        }
+        else
+        {
+            
+        }
+
+        isPressed = !isPressed;
+    }
+}
