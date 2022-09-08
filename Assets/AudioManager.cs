c19c4b68a87fb92e258aed35df03954b63610563
Romain <mromain911@gmail.com>
2022-09-08 10:33:41 +0200

    AudioManager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource playerAudio;
+    public AudioSource environementAudio;
+
+    public AudioClip hit;
+    public AudioClip movement;
+    public AudioClip getPatient;
+    public AudioClip victory;
+    
+    
+    void Start()
+    {
+        playerAudio = gameObject.AddComponent<AudioSource>();
+        environementAudio = gameObject.AddComponent<AudioSource>();
+    }
+}
