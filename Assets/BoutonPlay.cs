0437c37cb498ef9c66e0e41ba18fa09398fd0070
Romain <mromain911@gmail.com>
2022-09-09 11:35:21 +0200

    MainMenu finish

--- Assets/BoutonPlay.cs
+++ Assets/BoutonPlay.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BoutonPlay : MonoBehaviour
 {
+    public GameObject credit;
+    
     public int levelIndex;
     public void Load()
     {
         SceneManager.LoadScene(levelIndex);
+    }
+
+    public void OnCredit()
+    {
+        credit.gameObject.SetActive(true);
+    }
+    
+    public void OffCredit()
+    {
+        credit.gameObject.SetActive(false);
+    }
+    
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
